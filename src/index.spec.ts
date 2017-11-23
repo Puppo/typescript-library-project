82f32d8cd0f19c5d22b7851d9faabc11e71cc87a
Luca Del Puppo <luca@delpuppo.net>
2017-11-23 22:07:15 +0100

    Breaking: Use type guard for return type of isBoolean

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,20 +1,22 @@
 import { isBoolean } from './index'
 
-test('isBoolean should return true if the given value is an instance of Boolean', () => {
-  expect(isBoolean(new Boolean())).toEqual(true)
-})
+describe('isBoolean', () => {
+  test('should return true if the given value is an instance of Boolean', () => {
+    expect(isBoolean(new Boolean())).toEqual(true)
+  })
 
-test('isBoolean should return true if the given value is the literal `true`', () => {
-  expect(isBoolean(true)).toEqual(true)
-})
+  test('should return true if the given value is the literal `true`', () => {
+    expect(isBoolean(true)).toEqual(true)
+  })
 
-test('isBoolean should return true if the given value is the literal `false`', () => {
-  expect(isBoolean(false)).toEqual(true)
-})
+  test('should return true if the given value is the literal `false`', () => {
+    expect(isBoolean(false)).toEqual(true)
+  })
 
-test('isBoolean should return false if the given value is any non-boolean value', () => {
-  expect(isBoolean('')).toEqual(false)
-  expect(isBoolean(undefined)).toEqual(false)
-  expect(isBoolean(null)).toEqual(false)
-  expect(isBoolean(123)).toEqual(false)
+  test('should return false if the given value is any non-boolean value', () => {
+    expect(isBoolean('')).toEqual(false)
+    expect(isBoolean(undefined)).toEqual(false)
+    expect(isBoolean(null)).toEqual(false)
+    expect(isBoolean(123)).toEqual(false)
+  })
 })
